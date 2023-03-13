b5fe1eda53719c2c4d538ae1d3fdd01cc6764ca4
Glenn McDowell <mcdga002@mymail.unisa.edu.au>
2023-03-13 18:21:57 +1030

    Update Item.java
    Added age to start at 0 and set others as part of their constructor

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,18 @@
 package students.items;
 
 public class Item {
+	//All items start at age 0, with the maturation age, death age, and monetary value set as part of its constructor. 
+		private int age;
+		private int deathAge;
+		private int maturationAge;
+		private int monetaryValue;
+		
+		public Item(int age, int deathAge, int maturationAge, int monetaryValue) {
+			this.age = 0;
+			this.deathAge = deathAge;
+			this.monetaryValue = monetaryValue;
+			this.maturationAge = maturationAge;
+		}
 
 	
 }
